1c258c5487e2467a2721586f6b98c537b521ae11
Marisz17 <marisaninc@gmail.com>
2024-05-18 11:32:42 +0200

    add functions that get human and computer choices

---
+++ javascript.js
@@ -0,0 +1,34 @@
+
+
+//function that randomly returns “rock”, “paper” or “scissors”.
+
+let getComputerChoice = () => {
+    /* Declare a variable and assign it a random number between 3 choices*/
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    /* Link three choices 0, 1 and 2 to rock, paper and scissors respectively and return
+    the corresponding words i.e. switch*/
+
+    switch (randomNumber) {
+
+        case 0: 
+                return "rock";
+                break;
+        case 1: 
+                return "paper";
+                break;
+        case 2: 
+                return "scissors";
+                break;
+    }
+}
+
+//function that takes the user choice and returns it.
+
+let getHumanChoice = () => {
+    //prompt user choice
+    let userChoice = prompt("'rock', 'paper' or 'scissors'?")
+
+    //return user choice
+    return userChoice;
+}
